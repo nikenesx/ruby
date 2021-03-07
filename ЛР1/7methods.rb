cc229dbc51b924b9aac47d678928796bc762e2f8
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-07 23:58:20 +0300

    Add files via upload

--- ЛР1/7methods.rb
+++ ЛР1/7methods.rb
@@ -31,32 +31,22 @@ end
 
 # минимальная цифра числа
 def min(n, n2)
-  if n == 0 then
+  if n == 0
     return n2
   end
 
-  if n % 10 < n2 then
+  if n % 10 < n2
     return min(n/10, n % 10)
   else
     return min(n/10, n2)
   end
 end
 
-# вспомогательный метод, если число просто - true, else иначе
-def is_s1mple(v)
-  for i in 2..v-1 do
-    if v % i == 0 then
-      return false
-    end
-  end
-  return true
-end
-
 # наибольший простой делитель
 def maxd(v)
   maxdel = 1
   for i in 2..v-1 do
-    if v % i == 0 and is_s1mple(i) == true and i > maxdel then
+    if v % i == 0 and i.prime? == true and i > maxdel then
       maxdel = i
     end
   end
@@ -65,7 +55,7 @@ end
 
 # Найти произведение цифр числа, не делящихся на 5
 def proizvd(n)
-  if n < 10 and n != 5 then
+  if n < 10 and n != 5
     return n
   elsif n < 10 and n == 5
     return 0
@@ -80,38 +70,40 @@ end
 def f(num)
   max_d = 1
   for i in 1..num do
-    if num % i == 0 and is_s1mple(i) == false and i > max_d and i % 2 > 0
+    if num % i == 0 and i.prime? == false and i > max_d and i % 2 > 0
       max_d = i
     end
   end
   return i.gcd mn(num)
 end
 
-puts "Введите число: "
-num = gets.to_i()
-
-puts "Введите номер метода"
-v = gets
-puts v
-
-case v
-when "1"
-  puts sum(num)
-when "2"
-  puts mn(num)
-when "3"
-  put min(num, 10)
-when "4"
-  puts max(num, 0)
-when "5"
-  puts maxd(num)
-when "6"
-  puts proizvd(num)
-when "7"
-  puts f(num)
-else
-  puts "Hello world"
+def main(v)
+  print "Введите число: "
+  num = gets.to_i
+  puts v
+  case v
+  when 1
+    puts sum(num)
+  when 2
+    puts mn(num)
+  when 3
+    put min(num, 10)
+  when 4
+    puts max(num, 0)
+  when 5
+    puts maxd(num)
+  when 6
+    puts proizvd(num)
+  when 7
+    puts f(num)
+  else
+    puts "Hello world"
+  end
 end
 
-
+puts "Введите параметр\n1. Сумма цифр числа\n2. Произведение цифр числа\n3. Минимальная цифра числа
+4. максимальная цифра числа\n5. Наибольший простой делитель\n6. Произведение цифр числа, не делящихся на 5
+7. НОД максимального нечетного непростого делителя числа и прозведения цифр данного числа"
+v = gets.to_i
+main(v)
 
