3fe97bb0ee5d872ef67425533669aa39020b7f78
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-12 21:43:17 +0300

    Add files via upload

---
+++ ЛР2/ex6.rb
@@ -0,0 +1,34 @@
+def filear(filepath)
+  file = IO.read(filepath)
+  Array.new | file.split(' ')
+end
+
+def keyarr(n)
+  Array.new(n){ |index| gets}
+end
+
+def n3(i, arr)
+  arr[i] == a.max
+end
+
+arr = Array.new
+print "1. Считать список из файла\n2. Ввод с клавиатуры\n->"
+n = gets.to_i
+case n
+when 1
+  print "Введите путь к файлу-> "
+  fp = gets.strip
+  printf fp
+  arr = filear(fp)
+when 2
+  print "Введите количество элементов-> "
+  n = gets.to_i
+  arr = keyarr(n)
+else
+  puts "Ошибка"
+end
+
+print "Введите индекс элемента-> "
+i = gets.to_i
+puts n3(i, arr)
+
