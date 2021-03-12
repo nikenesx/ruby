6ed756f6766308833800f5c7febc34303bed0cac
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-12 13:35:22 +0300

    Add files via upload

--- ЛР2/ex3.rb
+++ ЛР2/ex3.rb
@@ -14,12 +14,20 @@ def var2(n)
   arr
 end
 
+def var3(n)
+  st = ""
+  for i in 0...n do
+    st.concat(gets)
+  end
+  Array.[](st.split(" "))
+end
+
 print("Введите количество элементов массива-> ")
 n = gets.to_i
 
 #arr = var1(n)
-arr = var2(n)
-#arr = var3(n)
+#arr = var2(n)
+arr = var3(n)
 #arr = var4(n)
 #arr = var5(n)
 puts arr
