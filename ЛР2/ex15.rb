6b088f8a9b0b5ec516ea5b2ab70ef8f956096778
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-20 02:12:26 +0300

    Задание 15 задача 5

--- ЛР2/ex15.rb
+++ ЛР2/ex15.rb
@@ -1,17 +1,41 @@
-f = File.new("lines.txt", "r:UTF-8")
-lines = f.readlines
-freeqA = 1.0/26.0
-differenceArr = Array.new
-mostOftenSym = Array.new
-for line in lines
-    uniqElmsInLine = line.chars.uniq
-    countOfRept = Array.new
-    uniqElmsInLine.find_all {|elem| countOfRept.push(line.chars.count(elem))}
-    a, b = countOfRept.zip(uniqElmsInLine).sort.reverse.transpose
-    freqSymInLine = (a[0] * 1.0)/(line.length * 1.0)
-    mostOftenSym.push(b[0])
-    differenceArr.push((freqSymInLine - freeqA).abs)
+def n3
+    f = File.new("lines.txt", "r:UTF-8")
+    lines = f.readlines
+    freeqA = 1.0/26.0
+    differenceArr = Array.new
+    mostOftenSym = Array.new
+    for line in lines
+        uniqElmsInLine = line.chars.uniq
+        countOfRept = Array.new
+        uniqElmsInLine.find_all {|elem| countOfRept.push(line.chars.count(elem))}
+        a, b = countOfRept.zip(uniqElmsInLine).sort.reverse.transpose
+        freqSymInLine = (a[0] * 1.0)/(line.length * 1.0)
+        mostOftenSym.push(b[0])
+        differenceArr.push((freqSymInLine - freeqA).abs)
+    end
+    a, b = differenceArr.zip(lines).sort.transpose
+    a, c = differenceArr.zip(mostOftenSym).sort.transpose
+    puts a, "---", b, '---', c
 end
-a, b = differenceArr.zip(lines).sort.transpose
-a, c = differenceArr.zip(mostOftenSym).sort.transpose
-puts a, "---", b, '---', c
+
+def n5
+    f = File.new("lines.txt", "r:UTF-8")
+    lines = f.readlines
+    freeqA = 1.0/26.0
+    differenceArr = Array.new
+    mostOftenSym = Array.new
+    for line in lines
+        uniqElmsInLine = line.chars.uniq
+        countOfRept = Array.new
+        uniqElmsInLine.find_all {|elem| countOfRept.push(line.chars.count(elem))}
+        a, b = countOfRept.zip(uniqElmsInLine).sort.reverse.transpose
+        freqSymInLine = (a[0] * 1.0)/(line.length * 1.0)
+        mostOftenSym.push(b[0])
+        differenceArr.push(Math.sqrt((freqSymInLine + freeqA)/2))
+    end
+    a, b = differenceArr.zip(lines).sort.transpose
+    a, c = differenceArr.zip(mostOftenSym).sort.transpose
+    puts a, "---", b, '---', c
+end
+
+n5
