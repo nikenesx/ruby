4d4dc3af276fb84c37a66637db0e6d547ff4855c
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-20 00:23:03 +0300

    Задание 11 задача 8

--- ЛР2/ex11.rb
+++ ЛР2/ex11.rb
@@ -14,3 +14,20 @@ def n3
     end
     puts count
 end
+
+def n8
+    line = "фбвгдabcd"
+    reg = Regexp.compile(/[a-z]/)
+    l = 'e'
+    count = 0
+    while l != nil
+        l = line[reg]
+        if l != nil
+            line = line[l.length..line.length]
+            count = count + 1
+        else
+            break
+        end
+    end
+    puts count
+end
