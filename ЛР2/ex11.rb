ff6b493478656bbeb185f0ae62b8591b6eb558a5
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-20 00:22:00 +0300

    Задание 11 задача 3

---
+++ ЛР2/ex11.rb
@@ -0,0 +1,16 @@
+def n3
+    line = "фбвгдabcd"
+    reg = Regexp.compile(/[а-я]/)
+    l = 'e'
+    count = 0
+    while l != nil
+        l = line[reg]
+        if l != nil
+            line = line[l.length..line.length]
+            count = count + 1
+        else
+            break
+        end
+    end
+    puts count
+end
