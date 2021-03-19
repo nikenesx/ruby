5d3dcfffa152a3ff8ca1b8659b079b5a0b08e0a3
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-20 00:27:54 +0300

    Задача 11

--- ЛР2/ex11.rb
+++ ЛР2/ex11.rb
@@ -31,3 +31,31 @@ def n8
     end
     puts count
 end
+
+def n16
+    line = "dfg dfg fg 54 thr23"
+    reg = Regexp.compile(/[0-9]/)
+    l = 'e'
+    sr = Array.new
+    while l != nil
+        l = line[reg]
+        if l != nil
+            line = line[l.length..line.length]
+            sr.push(l.to_i)
+        else
+            break
+        end
+    end
+    puts sr.min
+end
+
+puts "1. Задача 3\n2. Задача 8\n3. Задача 16\nВыберите номер задачи:"
+n = gets.strip.to_i
+case n
+when 1
+    n3
+when 2
+    n8
+when 3
+    n16
+end
