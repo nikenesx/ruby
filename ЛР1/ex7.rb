771ae57dcef7a43bf33be8a758df2dc9a9ac303c
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-27 01:48:01 +0300

    7 задание метод 2

--- ЛР1/ex7.rb
+++ ЛР1/ex7.rb
@@ -18,5 +18,14 @@ def maxd(v)
 	return maxdel
 end
 
-puts maxd(28)
-gets
+def proizvd(n)
+	if n < 10 and n != 5 then
+		return n
+	elsif n < 10 and n == 5
+		return 0
+	elsif n % 10 != 5
+		return n % 10 * proizvd(n / 10)
+	else
+		return proizvd(n / 10)
+	end
+end
