56234c620b85dec77e90c37a7474fe59ef224be3
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-27 01:49:10 +0300

    7 задание
    Все методы

--- ЛР1/ex7.rb
+++ ЛР1/ex7.rb
@@ -29,3 +29,22 @@ def proizvd(n)
 		return proizvd(n / 10)
 	end
 end
+
+# произведение цифр числа
+def mn(n)
+	if n < 10
+		return n
+	else
+		return n % 10 * mn(n / 10)
+	end
+end
+
+def f(num)
+	max_d = 1
+	for i in 1..num do
+		if num % i == 0 and is_s1mple(i) == false and i > max_d and i % 2 > 0
+			max_d = i
+		end
+	end
+	return i.gcd mn(num)
+end
