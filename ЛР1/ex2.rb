99458074be22ddd016b1f01f3dd1cebb8e880741
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-27 01:03:25 +0300

    Задание 2

--- ЛР1/ex2.rb
+++ ЛР1/ex2.rb
@@ -1,124 +1,247 @@
-print "Введите ваше имя: " #Вывод без переноса на следующую строку
-names = gets # ввод с клавиатуры
-printf "Привет, #{names}" # приветствие пользователя, вывод с переносом
+menu = "1"
 
-s1 = 'простая строка'
-x = 7
-y = 3
-s2 = "#{x} * #{y} = #{x * y}"
-puts s2 #то же что и printf, но также преобразует объет в строку.
+while menu != "0"
 
-s3 = %q[строка с “кавычками”]
-s4 = %Q:Это тоже строка со „специальными символами‟ \t\n:
+	puts "1. Вывести приветсвие
+2. Показать все методы класса String
+3. Выполнить методы класса String
+4. Примеры форматирования строк
+5. Переменные и константы"
 
-s = `ls -a` #строка в обратных кавычках посылается ОС в качестве команды, рузьтат записывается обратно в строку
+	menu = gets.strip
 
-#Повторяет строку
-"Hello " * 5 #=> "Hello Hello Hello "
+	case menu
+		when "1"
+			print "Введите ваше имя: " #Вывод без переноса на следующую строку
+			names = gets # ввод с клавиатуры
+			printf "Привет, #{names}" # приветствие пользователя, вывод с переносом
 
-#Сложение строк
-"Hello" + " world" #=> "Hello world"
+		when "2"
+			for method in String.methods
+				puts method
+			end
+			puts String.methods.count
 
-#-1 если левая строка больше правой, 0 если равны, 1 если правая больше левой
-"abc" <=> "abc" #=> 0
+		when "3"
+			#Повторяет строку
+			puts "\"Hello\" * 5\nВывод:"
+			puts "Hello" * 5
+			gets
 
-#true если равны, false иначе
-"abc" == "abd" #=> false
+			#Сложение строк
+			puts "\"Hello\" + \" world\nВывод:"
+			puts "Hello" + " wordl"
+			gets
 
-#Регистронезависимая версия <=>
-"abcdef".casecmp("ABCDEF") #=> 0
+			#-1 если левая строка больше правой, 0 если равны, 1 если правая больше левой
+			puts "\"abc\" <=> \"abc\"\nВывод:"
+			puts "abc" <=> "abc"
+			gets
 
-#Если не был передан аргумент и не параметр separator, то удаляется \r и \n
-"hope\n".chomp #=> "hope"
-"what?".chomp("t?") #=> "wha"
+			#true если равны, false иначе
+			puts "\"abc\" == \"abd\"\nВывод:"
+			puts "abc" == "abd"
+			gets
 
-#Удаляет последний символ. Если есть комбинация /r/n, то удаляется вся комбинация
-"cristal\n\r".chop #=> "cristal"
+			#Регистронезависимая версия <=>
+			puts "\"abcdef\".casecmp(\"ABCDEF\")\nВывод:"
+			puts "abcdef".casecmp("ABCDEF")
+			gets
 
-#возвращает односимвольную строку с начала строки
-"abcd".chr #=> "a"
-"bcd".chr #=> "b"
+			#Если не был передан аргумент и не параметр separator, то удаляется \r и \n, nil если не было изменений
+			puts "\"hope\\n\".chomp\nВывод:"
+			puts "hope\n".chomp
+			puts "\"what?\".chomp(\"t?\")\nВывод:"
+			puts "what?".chomp("t?")
+			gets
 
-#Возвращает кол-во символов
-"hello world".count "o" #=> 2
+			#Удаляет последний символ. Если есть комбинация /r/n, то удаляется вся комбинация
+			puts "\"cristal\\n\\r\".chop\nВывод:"
+			puts "cristal\n\r".chop
+			gets
 
-#Возвращае ткопию с удаленными символами, nil усли строка не была модифицирована
-"I like beer".delete("beer") #=> "I like "
+			#возвращает односимвольную строку с начала строки
+			puts "\"abcd\".chr\nВывод:"
+			puts "abcd".chr
+			puts "\"bcd\".chr\nВывод:"
+			puts "bcd".chr
+			gets
 
-#Разбивает строку используя значение аргумента, по умолчанию \n
-"My name is\nKolya".each {|s| p s} #=> "My name is\n"
-                                   #=> "Kolya"
+			#Возвращает кол-во символов
+			puts "\"hello world\".count \"o\"\nВывод:"
+			puts "hello world".count "o"
+			gets
 
-#Передает каждый символ строки в блок
-"salam".each_char {|s| print s} #=> s a l a m
+			#Возвращае ткопию с удаленными символами, nil усли строка не была модифицирована
+			puts "\"I like beer\".delete(\"er\")\nВывод:"
+			puts "I like beer".delete("er")
+			gets
 
-#Вставляет строку п оиндексу
-"ello".insert(0, "H") #=> "Hello"
+			#Передает каждый символ строки в блок
+			puts "\"salam\".each_char {|s| print s}\nВывод:"
+			"salam".each_char {|s| print s}
+			gets
 
-#вычисляется следующий элемент увеличением кода крайнего правого элемента строки на единицу.
-"abcd".next #=> "abce"
+			# Сопостовляет правило, nil если нет совпадений
+			puts "\"abcc\".match(/abc/)\nВывод:"
+			puts "abcc".match(/abc/)
+			gets
 
-#Возвращает новую строку в которой символы строки переставлены в обратном порядке
-"stressed".reverse   #-> "desserts"
+			#Вставляет строку п оиндексу
+			puts "\"ello\".insert(0, \"H\")\nВывод:"
+			puts "ello".insert(0, "H")
+			gets
 
-"kitchen".chars #=> ['k'. 'i', 't', 'c', 'h', 'e', 'n']
+			#вычисляется следующий элемент увеличением кода крайнего правого элемента строки на единицу.
+			puts "\"abcd\".next\nВывод:"
+			puts "abcd".next
+			gets
 
-"hello".tr('eo', '!') #=> "h!ll!"
+			#Возвращает новую строку в которой символы строки переставлены в обратном порядке
+			puts "\"stressed\".reverse\nВывод:"
+			puts "stressed".reverse
+			gets
 
-#Удаляет ведущие и замыкающие пробелы
-"    hello    ".strip   #-> "hello"
+			puts "\"kitchen\".chars\nВывод:"
+			p "kitchen".chars
+			gets
 
-# длина строки
-s5 = "операционная система"
-s5.length # 20
+			puts "\"hello\".tr('eo', '!')\nВывод:"
+			puts "hello".tr('eo', '!')
+			gets
 
-# проверить пустая ли строка
-s6 = ""
-s6.empty? # true
-s6 = "тут что-то есть"
-s6.empty? # false
+			#Удаляет ведущие и замыкающие пробелы, nil если не было изменений
+			puts "\"    hello    \".strip\nВывод:"
+			puts "    hello    ".strip
+			gets
 
-# доступ к символам в строке
-s7 = "Хороший день"
-s7.slice(0) # "Х"
-s7.slice(0, 1) # "Хо"
-s7.slice(0..4) # "Хорош"
+			# длина строки
+			puts "\"операционная система\".length\nВывод:"
+			puts "операционная система".length
+			gets
 
-s7[0] # "Х"
-s7[0, 1] # "Хо"
-s7[0..4] # "Хорош"
+			# проверить пустая ли строка
+			puts "\"\".empty?\nВывод:"
+			puts "".empty?
+			puts "\"When?\".empty?\nВывод:"
+			puts "When?".empty?
+			gets
 
-# верхний и нижний регистр
-s8 = "я хочу стать больше"
-s9 = "ПОМОГИТЕ"
-s8.upcase # "Я ХОЧУ СТАТЬ БОЛЬШЕ"
-s9.downcase # "помогите"
+			# доступ к символам в строке, nil если ненаход
+			puts "\"Хороший день\".slice(0)\nВывод:"
+			puts "Хороший день".slice(0)
+			puts "\"Хороший день\".slice(1..4)\nВывод:"
+			puts "Хороший день".slice(1..4)
+			gets
 
-# верхний регистр у первого символа
-s10 = "евгений"
-s10.capitalize # "Евгений"
+			puts "\"Хороший день\"[0]\nВывод:"
+			puts "Хороший день"[0]
+			puts "\"Хороший день\"[1..4]\nВывод:"
+			puts "Хороший день"[1..4]
+			gets
 
-# работа с отступами
-"манго".ljust(20) # "манго               "
-"манго".rjust(20) # "               манго"
-"манго".center(21) # "        манго        "
+			# верхний и нижний регистр
+			puts "\"я хочу стать больше\".upcase\nВывод:"
+			puts "я хочу стать больше".upcase
+			gets
 
-# удаление последнего символа
-"Новая строка\n".chop # "Новая строка"
+			puts "\"ПОМОГИТЕ\".downcase\nВывод:"
+			puts "ПОМОГИТЕ".downcase
+			gets
 
-# поиск символов в строке
-"программист".include?("о") # true
-"программист".include?("д") # false
+			# верхний регистр у первого символа
+			puts "\"евгений\".capitalize\nВывод:"
+			puts "евгений".capitalize
+			gets
 
-"программист".index("п") # 0
-"программист".index("огр") # 2
+			# работа с отступами
+			puts "\"манго\".ljust(20)\nВывод:"
+			puts "манго".ljust(20)
+			gets
 
-# замена символов
-s11 = "У меня есть деньги"
-s11.sub("меня","нас") # "У нас есть деньги""
+			puts "\"манго\".rjust(20)\nВывод:"
+			puts "манго".rjust(20)
+			gets
 
-count = 3 # локальная переменная
-$value = 130 # глобальная переменная
-Mounths = 12 # константа
+			puts "\"манго\".center(21)\nВывод:"
+			puts "манго".center(21)
+			gets
 
-gets
+			# поиск символов в строке
+			puts "\"программист\".include?(\"о\")\nВывод:"
+			puts "программист".include?("о")
+			puts "\"программист\".include?(\"д\")\nВывод:"
+			puts "программист".include?("д")
+			gets
+
+			#nil если нет совпадений
+			puts "\"программист\".index(\"о\")\nВывод:"
+			puts "программист".index("о")
+			gets
+
+			# замена символов
+			puts "\"У меня есть деньги\".sub(\"меня\",\"нас\")\nВывод:"
+			puts "У меня есть деньги".sub("меня","нас")
+			gets
+
+			puts "\"Hello world\".rindex('o')\nВывод"
+			puts "Hello world".rindex('o')
+			gets
+
+			puts "\"How are you?\".gsub(/(\\s*)/, "")\nВывод"
+			puts "How are you?".gsub(/(\s*)/, "")
+			gets
+
+			puts "\"abcd\".succ\nВывод:"
+			puts "abcd".succ
+			gets
+
+			puts "\"salam\".each_line('l') {|s| print s}\nВывод:"
+			"salam".each_line('l') {|s| print s}
+			gets
+
+			puts "\"What\".clear\nВывод:"
+			puts "What".clear
+			gets
+
+			puts "\"    how\".rstrip\nВывод:"
+			puts "    how".rstrip
+			gets
+
+			puts "\"how    \".lstrip\nВывод:"
+			puts "how    ".lstrip
+
+		when "4"
+			puts "puts 'простая строка'\nВывод:"
+			puts 'простая строка'
+			gets
+
+			puts "x = 7\ny = 3\ns2 = \"\#{x} * \#{y} = \#{x * y}\"\nputs s2\nВывод:"
+			x = 7
+			y = 3
+			s2 = "#{x} * #{y} = #{x * y}"
+			puts s2
+			gets
+
+			puts "puts %q[строка с “кавычками”]\nВывод:"
+			puts %q[строка с “кавычками”]
+			gets
+
+			puts "puts %Q:Это тоже строка со „специальными символами‟ \\t\\n:\nВывод:"
+			puts %Q:Это тоже строка со „специальными символами‟ \\t\\n:
+			gets
+
+			puts "s = `ruby -v`"
+			s = `ruby -v`
+			puts s
+
+		when "5"
+			puts "count = 3 - локальная переменная"
+			puts "$value = 130 - глобальная переменная"
+			puts "Mounths = 12 - константа"
+
+	end
+end
+
+
+
