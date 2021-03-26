72b38f7d27d398e1c411da810eaf3fa67c1999d7
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-27 01:06:01 +0300

    3 задание условный оператор if

---
+++ ЛР1/ex3.rb
@@ -0,0 +1,20 @@
+puts "Какай ваш любимый язык?"
+language = gets
+puts language
+case language
+when "ruby"
+    puts "Не подлизывайся"
+when "python"
+    puts "Слишком скучно, ruby его заменит"
+when "c++"
+    puts "Может и так, но с ruby интересней"
+when "pascal"
+    puts "o_O"
+else
+    "Ответ неверный! Скоро твоим любимым языком станет Ruby!"
+end
+
+x = 3
+puts "x = 3" if x == 3
+gets
+
