1eed41fac8c751f316e6d527bf6755e37688d1c1
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-03-27 01:10:33 +0300

    3 задание
    оператор case

--- ЛР1/ex3.rb
+++ ЛР1/ex3.rb
@@ -42,3 +42,18 @@ x = if a > 0 then b
 	else c end
 gets
 
+
+value = gets
+case value
+    when 1..10
+        puts "Число принадлежит промежутку [1;10]"
+    when 15
+        puts "Это число 15"
+    when "Антон"
+        puts "Ваше имя Антон!"
+    when String
+        puts "Вы ввели строку"
+    else
+        puts "Ничего не подошло"
+end
+gets
