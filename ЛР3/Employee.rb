213f9104f5bcd6242955fdee774cd121a04c639b
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-04-02 17:34:57 +0300

    Изменен класс Employee

--- ЛР3/Employee.rb
+++ ЛР3/Employee.rb
@@ -1,5 +1,4 @@
 class Employee
-
 	attr_accessor :name,
 				  :birthDate,
 				  :phoneNumber,
@@ -12,30 +11,39 @@ class Employee
 				  :position,
 				  :lastSalary
 
+
 	def initialize *args
 		if args.length == 8 or args.length == 11
+			@name = args[0]
+			@birthDate = args[1]
+			@phoneNumber = args[2]
+			@adress = args[3]
+			@email = args[4]
+			@pasport = args[5]
+			@specialty = args[6]
+			@workExperience = args[7]
 
-			if args[7] == '0'
-				@name = args[0]
-				@birthDate = args[1]
-				@phoneNumber = args[2]
-				@adress = args[3]
-				@email = args[4]
-				@pasport = args[5]
-				@specialty = args[6]
-				@workExperience = args[7]
-
-			elsif args[7].to_i > 0
+			if args[7].to_i > 0 and args[7].to_i < 80
 				@nameLastWork = args[8]
 				@position = args[9]
 				@lastSalary = args[10]
-			else
 
-				raise 'Неверно введето поле \'Опыт работы\'.'
+			elsif args[7].to_i < 0
+				raise 'Неверно введено поле \'Опыт работы\'.'
 			end
 
 		else
 			raise 'Неверное количество аргументов.'
+		end
+	end
+
+
+	def printEmployers
+		puts @name, @birthDate, @phoneNumber, @adress, @email, @pasport,
+			 @specialty, @workExperience
+
+		if @workExperience != "0"
+			puts @nameLastWork, @position, @lastSalary
 		end
 	end
 end
