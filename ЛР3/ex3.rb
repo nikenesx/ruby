11df0e94897b8e09df0f75353e55845e39be474b
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-04-02 18:22:56 +0300

    Задание 3

---
+++ ЛР3/ex3.rb
@@ -0,0 +1,7 @@
+require_relative 'Employee'
+
+employe1 = Employee.new("Евгений Жидков Алексеевич", "25.05.1997", " 8(918) 345-67  34",
+						"г. Краснодар ул. Ленина д.23", "evgesha@mail.ru", "0520 265395",
+						"Системный администратор", "0")
+
+puts(employe1.phoneNumber)
