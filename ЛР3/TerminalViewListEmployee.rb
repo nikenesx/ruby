14a69337da3c7079ef37250a6c8a4b58b2c5c3e6
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2021-04-09 19:11:24 +0300

    Добавлен метод для записи списка сотрудников в файл

--- ЛР3/TerminalViewListEmployee.rb
+++ ЛР3/TerminalViewListEmployee.rb
@@ -10,6 +10,8 @@ class TerminalViewListEmployee < TestEmployee
 
 	attr_accessor :list_of_emp
 
+	@@key = 'DOG'
+
 	def initialize
 		@list_of_emp = []
 	end
@@ -83,7 +85,6 @@ class TerminalViewListEmployee < TestEmployee
 				retry
 			end
 
-
 			if emp.workExperience != "0"
 				print "Введите название предыдущего места работы сотрудника->"
 				data = gets.strip
@@ -103,6 +104,71 @@ class TerminalViewListEmployee < TestEmployee
 			f = gets.strip
 			nextemp = false if f == "2" || f == "Нет"
 		end
+	end
+
+
+	def shifr(word)
+		alph = 'A/B*C+D-E!F?G&H0I1J2K3L4M5N6O7P8Q9R@S:T>U<V#W%X=YZ'.chars
+		word = word.delete(" ").chars
+		kk = @@key * (alph.length / @@key.length + 1)
+		kk = kk[0, word.length]
+		res = Array.new
+		for i in 0..word.length - 1
+			res.push((word[i].to_i + alph.index(kk[i]) % alph.length))
+		end
+		res2 = ''
+		res.each { |i| res2 += alph[i]}
+		return res2
+	end
+
+
+	def deshift(line)
+		alph = 'A/B*C+D-E!F?G&H0I1J2K3L4M5N6O7P8Q9R@S:T>U<V#W%X=YZ'.chars
+		line = line.chars
+		kk = @@key * (alph.length / @@key.length + 1)
+		kk = kk[0, line.length]
+		res = Array.new
+		for i in 0..line.length - 1
+			res.push((alph.index(line[i]) - alph.index(kk[i]) + 26) % 26)
+		end
+		res.join('')
+	end
+
+
+	def outinfile
+		file = File.open('employers.txt', 'w')
+		tempstr = ''
+		s = "Name".ljust(30) +
+			+ "Birth date".ljust(12) +
+			+ "Phone number".ljust(15) +
+			+ "Adres".ljust(25) +
+			+ "Email".ljust(20) +
+			+ "Pasport".ljust(12) +
+			+ "Specialty".ljust(15) +
+			+ "Work exxperience".ljust(18) +
+			+ "Last work name".ljust(25) +
+			+ "Position".ljust(15) +
+			+ "Salary".ljust(10) + "\n"
+		for elem in @list_of_emp
+			tempstr += elem.name.ljust(30)
+			tempstr += elem.birthDate.ljust(12)
+			tempstr += elem.phoneNumber.ljust(15)
+			tempstr += elem.adress.ljust(25)
+			tempstr += elem.email.ljust(20)
+			pass = shifr(elem.pasport)
+			tempstr += pass.ljust(12)
+			tempstr += elem.specialty.ljust(15)
+			tempstr += elem.workExperience.ljust(18)
+			if elem.workExperience != '0'
+				tempstr += elem.nameLastWork.ljust(25)
+				tempstr += elem.position.ljust(15)
+				tempstr += elem.lastSalary.ljust(10)
+			end
+			tempstr += "\n"
+			s += tempstr
+			tempstr = ""
+		end
+		file.write(s)
 	end
 
 end
